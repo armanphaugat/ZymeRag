24bf13e3c641e6476c642326515af06de17f10f1
Arman <armanphaugat20@gmail.com>
2026-08-20 18:23:55 +0530

    Optimised the Semantic Query

--- Query/SemanticQuery.py
+++ Query/SemanticQuery.py
@@ -15,19 +15,24 @@ class SemanticQuery:
     def __init__(self,uuid:list[str]):
         self.uuid=uuid
 
+    def load_and_search(self,path):
+        vectorstore = FAISS.load_local(
+            path, embedder.model, allow_dangerous_deserialization=True
+        )
+        return vectorstore.similarity_search_with_score(self.query, k=3)
+    
     async def get_semantic_chunks_fromFeed(self,per_uuid:int=5,top_k:int=5):
         result=[]
+        paths=[]
         for uuid in self.uuid:
             feed_path=f"{BASE_DIR}/Feed/{uuid}"
             pathexsistence=Path(feed_path)
             if pathexsistence.exists()!=True :
                 raise FileNotFoundError(f"This Directory Do not Exists {feed_path}")
-            vectorstore=FAISS.load_local(
-                f"Data/Feed/{uuid}",
-                embedder.model,
-                allow_dangerous_deserialization=True
-            )
-            chunks=await asyncio.to_thread(vectorstore.similarity_search_with_score,self.query,k=3)
+            paths.append(feed_path)
+        tasks=[asyncio.to_thread(self.load_and_search,path) for path in paths]
+        all_result=await asyncio.gather(*tasks)
+        for chunks in all_result:
             result.extend(chunks)
         if(len(result)>6):
             result.sort(key=lambda x:x[1])
